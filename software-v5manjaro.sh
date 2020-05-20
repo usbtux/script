b59bc30442996d65f759e8831f70805440140ec8
usbtux <65667011+usbtux@users.noreply.github.com>
2020-05-20 17:28:48 +0100

    Add files via upload
    Manjaro post install software - check out before running edit and if required.

---
+++ software-v5manjaro.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# original Author 	: Erik Dubois
+# Website : https://www.erikdubois.be
+# Website	: https://www.arcolinux.info
+# Author : Usbtux
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+#software from Manjaro Linux repositories
+
+#sudo pacman -S --noconfirm --needed thunderbird
+sudo pacman -S --noconfirm --needed audacity
+sudo pacman -S --noconfirm --needed dropbox
+sudo pacman -S --noconfirm --needed picard
+sudo pacman -S --noconfirm --needed bleachbit
+sudo pacman -S --noconfirm --needed keepassxc
+sudo pacman -S --noconfirm --needed obs-studio
+sudo pacman -S --noconfirm --needed transmission-gtk
+sudo pacman -S --noconfirm --needed soundconverter
+sudo pacman -S --noconfirm --needed shotcut
+sudo pacman -S --noconfirm --needed atom
+#sudo pacman -S --noconfirm --needed mugshot
+sudo pacman -S --noconfirm --needed bookworm
+sudo pacman -S --noconfirm --needed libreoffice-fresh-en-gb
+#sudo pacman -S --noconfirm --needed telegram-desktop
+#sudo pacman -S --noconfirm --needed discord
+sudo pacman -S --noconfirm --needed syncthing-gtk
+sudo pacman -S --noconfirm --needed gocryptfs
+sudo pacman -S --noconfirm --needed simplescreenrecorder
+sudo pacman -S --noconfirm --needed
+
+###############################################################################################
+
+echo "################################################################"
+echo "################### core software installed"
+echo "################################################################"
